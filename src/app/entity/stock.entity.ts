b3a86a1c33a2ad452f3e9e226bb74b53943a792e
dhirtiroy321 <dhriti@ovntech.com>
2025-08-21 15:56:03 +0530

    fixes

--- src/app/entity/stock.entity.ts
+++ src/app/entity/stock.entity.ts
@@ -1,50 +1,50 @@
-import { Column, CreateDateColumn, Entity, Index, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { Exchange } from "../model/finance.types";
-import { StockRealtime } from "./stock-realtime.entity";
+// import { Column, CreateDateColumn, Entity, Index, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+// import { Exchange } from "../model/finance.types";
+// import { StockRealtime } from "./stock-realtime.entity";
 
-@Entity({ name: 'stocks' })
-@Index(['symbol', 'exchange'], { unique: true })
-export class Stock {
-    @PrimaryGeneratedColumn('uuid')
-    id!: string;
-
-
-    @Column({ type: 'varchar', length: 160 })
-    name!: string;
+// @Entity({ name: 'stocks' })
+// @Index(['symbol', 'exchange'], { unique: true })
+// export class Stock {
+//     @PrimaryGeneratedColumn('uuid')
+//     id!: string;
 
 
-    @Column({ type: 'varchar', length: 64 })
-    symbol!: string;
-
-    @Column({ type: 'enum', enum: Exchange })
-    exchange!: Exchange;
-
-    @Column({ type: 'varchar', length: 120 })
-    sector!: string;
+//     @Column({ type: 'varchar', length: 160 })
+//     name!: string;
 
 
-    @Column({ type: 'decimal', precision: 18, scale: 4 })
-    purchasePrice!: string;
+//     @Column({ type: 'varchar', length: 64 })
+//     symbol!: string;
 
-    @Column({ type: 'int', unsigned: true })
-    quantity!: number;
+//     @Column({ type: 'enum', enum: Exchange })
+//     exchange!: Exchange;
+
+//     @Column({ type: 'varchar', length: 120 })
+//     sector!: string;
 
 
-    @Column({ type: 'varchar', length: 240, nullable: true })
-    note?: string | null;
+//     @Column({ type: 'decimal', precision: 18, scale: 4 })
+//     purchasePrice!: string;
+
+//     @Column({ type: 'int', unsigned: true })
+//     quantity!: number;
 
 
-    @OneToOne(() => StockRealtime, (rt) => rt.stock, {
-        cascade: true,
-        eager: true,
-    })
+//     @Column({ type: 'varchar', length: 240, nullable: true })
+//     note?: string | null;
 
-    @JoinColumn()
-    realtime?: StockRealtime;
 
-    @CreateDateColumn()
-    createdAt!: Date;
+//     @OneToOne(() => StockRealtime, (rt) => rt.stock, {
+//         cascade: true,
+//         eager: true,
+//     })
 
-    @UpdateDateColumn()
-    updatedAt!: Date;
-}
+//     @JoinColumn()
+//     realtime?: StockRealtime;
+
+//     @CreateDateColumn()
+//     createdAt!: Date;
+
+//     @UpdateDateColumn()
+//     updatedAt!: Date;
+// }
